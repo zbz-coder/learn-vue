f4c079283610c2e8b638043f0b48b462887e13b5
unknown <2168393741@qq.com>
2024-09-04 01:05:55 +0800

    构建build

--- build/utils.ts
+++ build/utils.ts
@@ -1,13 +1,25 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
+import { cwd } from "node:process";
+import { dirname, resolve } from "node:path";
+import { fileURLToPath } from "node:url";
 
-/** 启动`node`进程时所在目录的绝对路径 */
-const root: string = process.cwd();
-console.log(process.cwd());
+/** 启动`node`时所在目录的绝对路径 */
+const root: string = cwd();
 
 function pathResolve(dir = ".", metaUrl = import.meta.url) {
-  // 当前文件所在目录的绝对路径
+  // 当前文件目录的绝对路径
   const currentFileDir = dirname(fileURLToPath(metaUrl));
   console.log(currentFileDir);
+  // build 目录的绝对路径
+  const buildDir = resolve(currentFileDir);
+  console.log("buildDir: ", buildDir);
+  // 解析的绝对路径
+  const resolvedPath = resolve(currentFileDir, dir);
+  console.log("resolve: ", resolvedPath);
+
+  return resolvedPath.startsWith(buildDir)
+    ? fileURLToPath(metaUrl)
+    : resolvedPath;
 }
-pathResolve();
+console.log("res: ", pathResolve());
+
+export { root, pathResolve };
--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,10 @@
 import { defineConfig, loadEnv } from "vite";
+import { root, pathResolve } from "./build/utils";
+
+console.log("------------------");
+console.log(root);
+console.log(pathResolve());
+console.log(import.meta.url)
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode, command }) => {
